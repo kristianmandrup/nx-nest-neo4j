d19cc5fc366d5eeb26e95cf4281358ef6aca96ab
kristianmandrup <kmandrup@gmail.com>
2022-04-02 11:43:57 +0100

    add required package dependencies and init generator

---
+++ src/generators/init/lib/add-dependencies.ts
@@ -0,0 +1,69 @@
+import type { GeneratorCallback, Tree } from '@nrwl/devkit';
+import { addDependenciesToPackageJson, readJson } from '@nrwl/devkit';
+import { satisfies } from 'semver';
+import {
+  nestJsSchematicsVersion,
+  nestJsVersion7,
+  nestJsVersion8,
+  nxVersion,
+  reflectMetadataVersion,
+  rxjsVersion6,
+  rxjsVersion7,
+} from '../../../utils/versions';
+
+export function addDependencies(tree: Tree): GeneratorCallback {
+  // Old nest 7 and rxjs 6 by default
+  let NEST_VERSION = nestJsVersion7;
+  let RXJS = rxjsVersion6;
+
+  const packageJson = readJson(tree, 'package.json');
+
+  if (packageJson.dependencies['@angular/core']) {
+    let rxjs = packageJson.dependencies['rxjs'];
+
+    if (rxjs.startsWith('~') || rxjs.startsWith('^')) {
+      rxjs = rxjs.substring(1);
+    }
+
+    if (satisfies(rxjs, rxjsVersion7)) {
+      NEST_VERSION = nestJsVersion8;
+      RXJS = packageJson.dependencies['rxjs'];
+    }
+  } else {
+    NEST_VERSION = nestJsVersion8;
+    RXJS = rxjsVersion7;
+  }
+
+  return addDependenciesToPackageJson(
+    tree,
+    {
+      '@nestjs/common': NEST_VERSION,
+      '@nestjs/core': NEST_VERSION,
+      '@nestjs/platform-express': NEST_VERSION,
+      'reflect-metadata': reflectMetadataVersion,
+      // specific
+      '@nestjs/jwt': NEST_VERSION,
+      '@nestjs/config': NEST_VERSION,
+      'neo4j-driver': '*',
+      'graphql': '*',
+      'graphql-tools': '*',
+      'graphql-type-json': '*',
+      'jsonwebtoken': '*',
+      'bcrypt': '*',
+      'uuid': '*',
+      '@neo4j/graphql-ogm': '*',
+      'apollo-server-plugin-base': '*',
+      '@koakh/nestjs-package-jwt-authentication-graphql': '*',
+      'apollo-server-core': '*',
+      'subscriptions-transport-ws': '*',
+      'class-validator': '*',
+      rxjs: RXJS,
+      tslib: '^2.0.0',
+    },
+    {
+      '@nestjs/schematics': nestJsSchematicsVersion,
+      '@nestjs/testing': NEST_VERSION,
+      '@nrwl/nest': nxVersion,
+    }
+  );
+}
